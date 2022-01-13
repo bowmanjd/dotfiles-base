ec300e280959dfea0b2a38a3a3dbe314be85fd54
Jonathan Bowman <jbowmancargas@users.noreply.github.com>
2022-01-12 19:50:54 -0500

    fix: fixed config file globbing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,3 @@
-for filename in vim.fn.globpath(vim.fn.stdpath('config'), '*.lua'):gmatch("([^/.]+).lua") do
-  if(filename ~= 'init')
-  then
-    require(filename)
-  end
+for filename in vim.fn.globpath(vim.fn.stdpath('config')..'/lua', '*.lua'):gmatch("([^/.]+)%.lua") do
+  require(filename)
 end
