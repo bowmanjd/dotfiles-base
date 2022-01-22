84bb6af4c478be4139ed264db609118e28640b26
Jonathan Bowman <jbowmancargas@users.noreply.github.com>
2022-01-22 06:07:32 -0500

    style: formatting

--- .config/nvim/lua/0base.lua
+++ .config/nvim/lua/0base.lua
@@ -1,8 +1,8 @@
 vim.opt.hidden = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
-vim.opt.fileformat = 'unix'
-vim.opt.fileformats= {'unix','dos'}
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = { "unix", "dos" }
 
 vim.opt.backup = false
 vim.opt.writebackup = false
@@ -14,13 +14,14 @@ vim.opt.shiftwidth = 2
 
 vim.opt.number = true
 vim.opt.cursorline = true
-vim.opt.clipboard = 'unnamed'
+vim.opt.clipboard = "unnamed"
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.background = 'dark'
+vim.opt.background = "dark"
 
-vim.cmd 'au BufNewFile,BufRead *.md set spell spelllang=en_us ft=markdown formatoptions=l lbr wrap textwidth=0 wrapmargin=0 nolist'
-vim.cmd 'au BufNewFile,BufRead ssh_config,*/.ssh/config.d/*  setf sshconfig'
-vim.cmd 'au BufNewFile,BufRead *.sql set shiftwidth=4 tabstop=4 expandtab ff=unix'
+vim.cmd("au BufNewFile,BufRead *.md set spell spelllang=en_us ft=markdown formatoptions=l lbr wrap textwidth=0 wrapmargin=0 nolist")
+vim.cmd("au BufNewFile,BufRead ssh_config,*/.ssh/config.d/*  setf sshconfig")
+vim.cmd("au BufNewFile,BufRead *.sql set shiftwidth=4 tabstop=4 expandtab ff=unix")
+vim.cmd("au BufNewFile,BufRead *.js set shiftwidth=4 tabstop=4 expandtab")
